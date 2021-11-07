c2dbf75ba13d9ff0a87bdab638cd39e4bc99dd1f
EC2 Default User <ec2-user@ip-172-31-28-161.us-east-2.compute.internal>
2021-11-07 18:49:11 +0000

    sign up html and route done without controller

---
+++ calm/app/controllers/signup_controller.rb
@@ -0,0 +1,4 @@
+class SignupController < ApplicationController
+  def create
+  end
+end
--- calm/config/routes.rb
+++ calm/config/routes.rb
@@ -1,4 +1,6 @@
 CALM::Application.routes.draw do
+  get 'signup/create'
+
   resources :announcements
   # map '/' to be a redirect to '/announcements'
   root :to => redirect('/announcements')
