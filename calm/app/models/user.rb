75bc98308c3b44ebbd0e8f61f42d44262f7f969f
EC2 Default User <ec2-user@ip-172-31-28-161.us-east-2.compute.internal>
2021-11-12 00:20:34 +0000

    add validation for sign up form

--- calm/app/models/user.rb
+++ calm/app/models/user.rb
@@ -10,4 +10,6 @@ class User < ActiveRecord::Base
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+    validates :phone, presence: true, numericality: { only_integer: true }
+    validates :organization, presence: true
 end
