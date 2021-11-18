1f95ea6bfc90de609a0c1cd379680a64e45ef819
EC2 Default User <ec2-user@ip-172-31-47-183.ec2.internal>
2021-11-18 22:34:16 +0000

    fix login/out

--- calm/app/controllers/sessions_controller.rb
+++ calm/app/controllers/sessions_controller.rb
@@ -5,11 +5,7 @@ class SessionsController < ApplicationController
     
     
     def create
-<<<<<<< HEAD
         @user = User.find_by(email: params[:session][:email]) #I guss this already does the sanitization? (Based on: https://stackoverflow.com/questions/21886170/best-way-to-go-about-sanitizing-user-input-in-rails)
-=======
-        @user = User.find_by(email: params[:session][:email])
->>>>>>> fa61f1c8a07f907d802571f395f7a530e90a24b0
         if @user && @user.authenticate(params[:session][:password])
             session[:user_id] = @user.id
             flash[:notice] = 'Login successful'
@@ -20,14 +16,11 @@ class SessionsController < ApplicationController
         end
     end
     
-<<<<<<< HEAD
     def destroy
         session[:user_id] = nil
         flash[:notice] = 'Successfully logged out'
         redirect_to '/'
     end
     
-=======
->>>>>>> fa61f1c8a07f907d802571f395f7a530e90a24b0
     
 end
