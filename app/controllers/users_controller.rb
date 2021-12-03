53e307a036e4f263e04d701539c78144eb53aac7
kevin0748 <kevin0748@gmail.com>
2021-12-03 04:53:58 +0000

    fix logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,41 +3,37 @@ class UsersController < ApplicationController
         @user=User.new
     end
     
-
-    
     def edit
         current_user
-        if @current_user != nil
-            @select_research_interest = @current_user.research_interest_ids
-        else
-            flash.now[:alert] = @user.errors.full_messages
-             render :edit
-         end
+        
+        if @current_user == nil
+            redirect_to root_path
+            return
+        end
+        
+        @select_research_interest = @current_user.research_interest_ids
+
     end
     
     def update
         current_user
-        if @current_user.valid?
-            if @current_user.update(user_params)
-                redirect_to edit_user_path, notice: 'User information was successfully updated.'
-            else
-                render :edit
         
-            end
-         else
-            flash.now[:alert] = @user.errors.full_messages
-             render :edit
+        if @current_user == nil
+            redirect_to root_path
+            return
         end
-      end
-             
-    
+        
+        if @current_user.update(user_params)
+            redirect_to edit_user_path, notice: 'User information was successfully updated.'
+        else
+            flash.now[:alert] = @current_user.errors.full_messages
+            render :edit
+        end
+    end
     
     private
-  def user_params
-
-    params.require(:user).permit(:account_name, :password, :password_confirmation, :first_name, :last_name, :organization, :email, :phone, :mentor, :mentee, research_interest_ids:[])
-  end
-    
-
+    def user_params
+        params.require(:user).permit(:account_name, :password, :password_confirmation, :first_name, :last_name, :organization, :email, :phone, :mentor, :mentee, research_interest_ids:[])
+    end
     
 end
