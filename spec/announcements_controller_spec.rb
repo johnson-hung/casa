f4cbf3a91353a9a2d8f3e036adad6a8a258cdb4f
EC2 Default User <ec2-user@ip-172-31-68-120.ec2.internal>
2021-12-11 21:58:03 +0000

    added announcement TDD

--- spec/announcements_controller_spec.rb
+++ spec/announcements_controller_spec.rb
@@ -18,73 +18,63 @@ end
 
 describe AnnouncementsController, :type => :controller do
 
-    describe 'Show' do
-        let!(:announcement) {Announcement.create!(title: 'Testing Announcement Create') }
+  describe 'Show' do
+    let!(:announcement) {Announcement.create!(title: 'Testing Announcement Create') }
 
-        it 'should show the detail of the announcement' do
-            get :show, id: announcement.id
-            expect(response).to render_template('show')
-        end
+    it 'should show the detail of the announcement' do
+        get :show, id: announcement.id
+        expect(response).to render_template('show')
+    end
+  end
+  
+  describe 'GET Index' do
+    it 'display index.html' do
+        get :index
+        expect(response).to render_template('index')
+    end
+  end
+  
+  describe 'Create' do
+    it 'creates a new announcement and redirects to the CALM index page' do
+      organizer_count = Announcement.all.count
+      post :create, announcement: {:title=> "Testing", :author=>"Q"}
+      expect(Announcement.all.count).to eq(organizer_count+1)
+      expect(response).to redirect_to(announcements_path)
+    end
+  end    
+  
+  describe 'Edit' do
+    let!(:announcement) {Announcement.create!(title: 'Testing', author: 'Q')}
+    
+    before do 
+      get :edit, id: announcement.id
     end
     
+    it 'should edit an announcement and redirect' do
+      expect(assigns(:announcement)).to eql(announcement)
+      expect(response).to render_template('edit')
+    end
+  end  
+  
+  describe 'Update' do
+    let!(:announcement){ Announcement.create!(title: 'Testing', author: 'Q')}
     
-    # describe 'Create' do
-    #     it 'creates a new movie and redirects to the movie index page' do
-    #         orig_movies_count = Movie.all.count
-    #         post :create, movie: {:title=>"A hole new world", :director=>"Peter Wu"}
-    #         expect(Movie.all.count).to eq(orig_movies_count + 1)
-
-    #         expect(response).to redirect_to(movies_url)
-    #     end
-    # end
-    
-    
-    # describe 'Edit' do
-    #     let!(:movie) { Movie.create!(title: 'OX', director: 'Peter Wu') }
-
-    #     before do
-    #         get :edit, id: movie.id
-    #     end
-
-    #     it 'should edit a movie and redirect' do
-    #         expect(assigns(:movie)).to eql(movie)
-    #         expect(response).to render_template('edit')
-    #     end
-    # end
-    
-    # describe 'Update' do
-    #     let!(:movie) { Movie.create!(title: 'OX', director: 'Peter Wu') }
-
-    #     it 'updates a movie and redirects to the movie page' do
-    #         put :update, id: movie.id, movie: {:title=>"OX"}
-    #         movie.reload
-    #         expect(movie.title).to eql('OX')
-
-    #         expect(response).to redirect_to(movie_path(movie))
-    #         expect(flash[:notice]).to eq("OX was successfully updated.")
-    #     end
-    # end
-    
-    # describe 'DELETE' do
-    #     let!(:movie) { Movie.create!(title: 'OX', director: 'Peter Wu') }
-
-    #     it 'deletes a movie and redirects to the home page and alert' do
-    #         orig_movies_count = Movie.all.count
-    #         delete :destroy, id: movie.id
-    #         expect(Movie.all.count).to eq(orig_movies_count - 1)
-
-    #         expect(response).to redirect_to(movies_path)
-    #         expect(flash[:notice]).to eq("Movie 'OX' deleted.")
-    #     end
-    # end
-    
-    
-    # describe 'GET Index' do
-    #     it 'display index.html' do
-    #         get :index
-    #         expect(response).to render_template('index')
-    #     end
-    # end
-
-
+    it 'Update an announcement and redirect to the home page' do
+      put :update, id: announcement.id, announcement: {:title=>'Changing Title'}
+      announcement.reload
+      expect(announcement.title).to eql('Changing Title')
+    end
+  end
+  
+  describe 'DELETE' do
+    let!(:announcement){ Announcement.create!(title: 'Testing', author: 'Q')}
+    it 'Delete an announcement and redirect to the home page' do
+      organizer_count = Announcement.all.count
+      delete :destroy, id: announcement.id
+      expect(Announcement.all.count).to eql(organizer_count-1)
+      
+      expect(response).to redirect_to(announcements_path)
+    end
+  end
+  
 end
